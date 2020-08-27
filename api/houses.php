df08efc8f59be367d602d02c9d2ffac0c43a277d
Phil Ewels <phil.ewels@scilifelab.se>
2020-08-28 00:53:08 +0200

    Start building filters

--- api/houses.php
+++ api/houses.php
@@ -24,43 +24,75 @@ if ($mysqli->connect_errno) {
    die("Failed to connect to MySQL: " . $mysqli->connect_error);
 }
 
-// Get houses
-$sql = '
-  SELECT *
-  FROM house_details
-  INNER JOIN search_result_urls ON house_details.id = search_result_urls.id
-  LEFT JOIN geocoding_results ON house_details.id = geocoding_results.house_id;
-';
+require_once('geocode_address.php');
+require_once('ratings.php');
+require_once('comments.php');
+require_once('tags.php');
+
+// AngularJS POST data looks weird
+$postdata = json_decode(file_get_contents("php://input"), true);
+if(is_null($postdata)) $postdata = [];
+$postdata = array_merge($_GET, $postdata);
+
+///
+/// NB: I should do all of this in joins but it's late and I'm a bad person.
+///
+
+// Get search results
+$sql = 'SELECT `id`, `url` FROM `search_result_urls`';
 $results = [];
 if ($result = $mysqli->query($sql)) {
-  while ($row = $result->fetch_assoc()) {
-    $results[$row['house_id']] = $row;
+  while ($row = $result->fetch_row()) {
+    $results[$row[0]] = array('house_id' => $row[0], 'url' => $row[1]);
   }
   $result->free_result();
 }
 
-// Unencode JSON strings
+// Go over each house
 foreach($results as $house_id => $house){
+
+  // Get house details
+  $sql = 'SELECT * FROM house_details WHERE `id` = "'.$mysqli->real_escape_string($house_id).'"';
+  if ($result = $mysqli->query($sql)) {
+    while ($row = $result->fetch_assoc()) {
+      $results[$house_id] = array_merge($results[$house_id], $row);
+    }
+    $result->free_result();
+  }
+
+  // Unencode JSON strings
   $results[$house_id]['address'] = @json_decode($results[$house_id]['address']);
   $results[$house_id]['data_layer'] = @json_decode($results[$house_id]['data_layer']);
+
+  // Total living area
+  $results[$house_id]['size_total'] = @$results[$house_id]['living_area'] + @$results[$house_id]['supplemental_area'];
+
 }
 
-// Get ratings
-require_once('ratings.php');
 foreach($results as $house_id => $house){
+  $remove = false;
+  // FILTERS
+  if(isset($postdata['price_min']) && @$house['price'] < $postdata['price_min']) $remove = true;
+  if(isset($postdata['price_max']) && @$house['price'] > $postdata['price_max']) $remove = true;
+  if(isset($postdata['size_total_min']) && $house['size_total'] < $postdata['size_total_min']) $remove = true;
+
+  if($remove) unset($results[$house_id]);
+}
+
+foreach($results as $house_id => $house){
+
+  // Get geocode results
+  $results[$house_id] = array_merge($results[$house_id], geocode_address(null, $house_id));
+
+  // Get ratings
   $results[$house_id]['ratings'] = get_house_ratings($house_id);
-}
 
-// Get comments
-require_once('comments.php');
-foreach($results as $house_id => $house){
+  // Get comments
   $results[$house_id]['comments'] = get_house_comments($house_id);
-}
 
-// Get tags
-require_once('tags.php');
-foreach($results as $house_id => $house){
+  // Get tags
   $results[$house_id]['tags'] = get_house_tags($house_id);
+
 }
 
-echo json_encode(array("status"=>"success", "results"=>$results), JSON_PRETTY_PRINT);
+echo json_encode(array("status"=>"success", "num_results"=>count($results), "results"=>$results), JSON_PRETTY_PRINT);
