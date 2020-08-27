b28a5d426ef1d9a94838c170bf6064cd3e935365
Phil Ewels <phil.ewels@scilifelab.se>
2020-08-27 22:54:52 +0200

    Button to add a new tag

--- api/tags.php
+++ api/tags.php
@@ -37,7 +37,7 @@ function get_house_tags($house_id){
   return $results;
 }
 
-// Save tag
+// Save tag status for a house
 function save_house_tag($house_id, $tag_id, $selected){
   global $mysqli;
 
@@ -56,6 +56,17 @@ function save_house_tag($house_id, $tag_id, $selected){
           `tag_id` = "'.$mysqli->real_escape_string($tag_id).'"
     ';
     $mysqli->query($sql);
+  }
+}
+
+// Save a new tag
+function save_new_tag($new_tag){
+  global $mysqli;
+  $sql = 'INSERT INTO `tags` (`tag`) VALUES ("'.$mysqli->real_escape_string($new_tag).'")';
+  if ($mysqli->query($sql) === TRUE) {
+    return array('status' => 'success', 'new_tag_id' => $mysqli->insert_id);
+  } else {
+    return array('status' => 'error', 'error_msg' => $mysqli->error);
   }
 }
 
@@ -82,13 +93,17 @@ if ( basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"]) ) {
   // AngularJS POST data looks weird
   $postdata = json_decode(file_get_contents("php://input"), true);
 
-  // Return ratings results
+  // Return tags for a house
   if(isset($_GET['house_id']) && strlen($_GET['house_id']) > 0){
     echo json_encode(get_house_tags($_GET['house_id']), JSON_PRETTY_PRINT);
   }
-  // Save ratings results
+  // Save a tag status for a house
   else if(is_array($postdata) && isset($postdata['house_id']) && isset($postdata['tag_id']) && isset($postdata['selected'])){
     echo json_encode( save_house_tag($postdata['house_id'],$postdata['tag_id'],$postdata['selected']), JSON_PRETTY_PRINT);
+  }
+  // Save a new tag
+  else if(is_array($postdata) && isset($postdata['new_tag'])){
+    echo json_encode( save_new_tag($postdata['new_tag']), JSON_PRETTY_PRINT);
   }
   // No action - give error status
   else {
