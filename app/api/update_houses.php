73d961002a816e438b582109676ca34182d28ecb
Phil Ewels <phil.ewels@scilifelab.se>
2020-09-24 00:01:15 +0200

    Move new GraphQL code into main file.

--- app/api/update_houses.php
+++ app/api/update_houses.php
@@ -6,98 +6,175 @@
 //////////////////////////////////////////////
 
 /**
- * Scrape the Hemnet "Saved Search" pagesu / JSON
+ * Use the Hemnet GraphQL API to pull house details from "Saved Searches"
  */
 
 // Fetch search results from Hemnet
-function fetch_hemnet_houses($search_id = false){
+function fetch_hemnet_houses(){
   global $mysqli;
 
-  $search_ids = [];
-  // Search IDs are supplied
-  if($search_id){
-    $search_ids = explode(",", $search_id);
-    $search_ids = array_map("trim",$search_ids);
+  // Get the saved-search details from the DB
+  require_once('saved_searches.php');
+  $saved_searches = fetch_saved_searches();
+  if($saved_searches['status'] !== 'success'){
+    return array("status"=>"error", "msg" => "Could not fetch saved searches!", "saved_searches" => $saved_searches);
   }
-  // Fetch search IDs from the database
-  else {
-    $sql = 'SELECT `search_id` FROM `saved_searches`';
-    if ($result = $mysqli->query($sql)) {
-      while ($row = $result->fetch_row()) {
-        $search_ids[] = $row[0];
-      }
-      $result->free_result();
-    }
+  if(count($saved_searches['saved_searches']) == 0){
+    return array("status"=>"no_saved_searches", "msg" => "No saved search IDs found! Please add some in the 'Map' panel.");
   }
-  if(count($search_ids) == 0) return array("status"=>"no_saved_searches", "msg" => "No saved search IDs found! Please add some in the 'Map' panel.");
 
   $houses = [];
-  foreach($search_ids as $search_id){
 
-    //// Fetch search key from web page
-    // We could do this once and save it in the database, but it changes if anything in the
-    // saved search is changed on Hemnet. So to be safe, we fetch it every time.
-    $search_key = false;
-    $hn_url = "https://www.hemnet.se/bostader?subscription=$search_id";
-    $html_page = @file_get_contents($hn_url);
-    if($html_page === FALSE) return array("status"=>"error", "msg" => "Could not fetch search page: $hn_url");
-    preg_match('/&quot;search_key&quot;:&quot;([\w]+)&quot;/', $html_page, $matches);
-    if($matches){
-      $search_key = $matches[1];
-    }
-    if(!$search_key) return array("status"=>"error", "msg" => "Could not find search_key: $hn_url");
-    if(strlen($search_key) < 20) return array("status"=>"error", "msg" => "Search key looks wrong: $search_key (from $hn_url)");
+  $str_keys = [
+    'id',
+    'type'
+  ];
+  $bool_keys = [
+    'isUpcoming',
+    'isNewConstruction',
+    'isForeclosure',
+    'isBiddingOngoing',
+  ];
+  $num_keys = [
+    'livingArea',
+    'landArea',
+    'supplementalArea',
+    'daysOnHemnet',
+    'numberOfRooms'
+  ];
 
-    // Load the hemnet JSON
-    $hn_json_url = "https://www.hemnet.se/bostader/search/$search_key";
-    $json_raw = @file_get_contents($hn_json_url);
-    if($json_raw === FALSE) return array("status"=>"error", "msg" => "Could not fetch hemnet JSON: $hn_json_url <br>".print_r(error_get_last(), true));
-    $hn_results = @json_decode($json_raw, true);
-    if(is_null($hn_results)) return array("status"=>"error", "msg" => "Could not decode hemnet JSON: $hn_json_url <br>".print_r(error_get_last(), true));
-    if(!isset($hn_results['properties'])) return array("status"=>"error", "msg" => "Properties not found in hemnet JSON: $hn_json_url");
+  // Kick off API calls for each saved search
+  foreach($saved_searches['saved_searches'] as $saved_search){
 
-    // Flatten / parse info from each house ready to go into the db
-    $str_keys = [
-      'id',
-      'address',
-      'location_name',
-      'typeSummary',
-      'age',
-      'url',
-      'locations_string'
-    ];
-    $boolean_keys = [
-      'project',
-      'new_construction',
-      'ongoing_bidding',
-      'foreclosure',
-      'upcoming',
-      'has_price_change',
-      'deactivated_before_open_house',
-      'should_display_showings'
-    ];
-    foreach($hn_results['properties'] as $house){
-      $this_house = array();
-      foreach($str_keys as $key){
-        $this_house[$key] = $house[$key];
+    $api_limit = 200;
+    $api_offset = 0;
+    $num_results = false;
+
+    // We can't fetch all results in one query, so we need to loop through the offset pages
+    while($num_results === false || $api_offset < $num_results){
+
+      $postdata = array(
+        'query' => graphql_iOSSearchQuery(),
+        'variables' => array(
+          "sort" => "NEWEST",
+          "limit" => $api_limit,
+          "offset" => $api_offset,
+        )
+      );
+
+      $postdata['variables']['searchInput'] = array();
+      // We need this, otherwise we get all of Sweden
+      $postdata['variables']['searchInput']['locationIds'] = [];
+
+      // Build query from saved search metadata
+      foreach($saved_search['search'] as $k => $v){
+        // Empty fields
+        if(is_null($v)) continue;
+        if(is_array($v) && count($v) == 0) continue;
+        if(is_string($v) && strlen($v) == 0) continue;
+        // Location IDs
+        if($k == 'locations'){
+          foreach($v as $loc){
+            $postdata['variables']['searchInput']['locationIds'][] = $loc['id'];
+          }
+          continue;
+        }
+        // Other fields
+        $postdata['variables']['searchInput'][$k] = $v;
       }
-      foreach($boolean_keys as $key){
-        $this_house[$key] = $house[$key] ? 1 : 0;
-      }
-      // More complicated ones
-      $this_house['lat'] = $house['coordinate'][0];
-      $this_house['lng'] = $house['coordinate'][1];
-      $this_house['price'] = _clean_num($house['price']);
-      $this_house['rooms'] = _clean_num($house['rooms']);
-      $this_house['land_area'] = _clean_num($house['land_area']);
-      $this_house['living_area'] = _clean_num($house['living_space']['table']['living_area']);
-      $this_house['supplemental_area'] = _clean_num($house['living_space']['table']['supplemental_area']);
-      $this_house['size_total'] = @intval($this_house['living_area']) + @intval($this_house['supplemental_area']);
-      $this_house['image_url'] = str_replace('itemgallery_M', 'itemgallery_cut', $house['medium_image_url']);
-      $this_house['created'] = time();
 
-      // Assoc ID with key as house ID in case a house comes up more than once, prevents duplication
-      $houses[$house['id']] = $this_house;
+      // Build POST API call using cURL
+      $api_url = 'https://www.hemnet.se/graphql';
+      $curl = curl_init($api_url);
+      curl_setopt($curl, CURLOPT_HEADER, false);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+        'Content-type: application/json',
+        'Accept: */*'
+      ));
+      curl_setopt($curl, CURLOPT_POST, true);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($postdata));
+
+      // Fetch cURL API response
+      $result_raw = curl_exec($curl);
+      $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+      if($status != 200){
+        return array(
+          'status'=>'error',
+          'msg' => 'Hemnet GraphQL API call failed',
+          'curl_status' => $status,
+          'curl_error' => curl_error($curl),
+          'curl_errno' => curl_errno($curl),
+          'url' => $api_url,
+          'result' => @json_decode($result_raw, true),
+          'result_raw' => $result_raw
+        );
+      }
+      curl_close($curl);
+
+      // Parse the response
+      $results_json = @json_decode($result_raw, true);
+      $num_results = $results_json['data']['searchListings']['total'];
+
+      foreach($results_json['data']['searchListings']['listings'] as $listing){
+
+        $this_house = [];
+
+        foreach($str_keys as $k){
+          if(!array_key_exists($k, $listing)) {
+            $this_house[$k] = '';
+            continue;
+          }
+          $this_house[$k] = @$listing[$k];
+        }
+        foreach($bool_keys as $k){
+          if(!array_key_exists($k, $listing)) {
+            $this_house[$k] = 0;
+            continue;
+          }
+          $this_house[$k] = @$listing[$k] ? 1 : 0;
+        }
+        foreach($num_keys as $k){
+          if(!array_key_exists($k, $listing)) {
+            $this_house[$k] = 0;
+            continue;
+          }
+          $this_house[$k] = is_null($listing[$k]) ? 0 : $listing[$k];
+        }
+
+        // Array keys two-deep
+        $this_house['lat'] = $listing['coordinates']['lat'];
+        $this_house['lng'] = $listing['coordinates']['long'];
+        $this_house['askingPrice'] = 0;
+        if(array_key_exists('askingPrice', $listing) && !is_null($listing['askingPrice'])) $this_house['askingPrice'] = $listing['askingPrice']['amount'];
+        $this_house['runningCosts'] = 0;
+        if(array_key_exists('runningCosts', $listing) && !is_null($listing['runningCosts'])) $this_house['runningCosts'] = $listing['runningCosts']['amount'];
+
+        // Closest visning date
+        $this_house['nextOpenHouse'] = null;
+        $upcomingOpenHouses = [];
+        if(array_key_exists('upcomingOpenHouses', $listing)){
+          foreach($listing['upcomingOpenHouses'] as $oh){
+            $timestamp = floatval($oh['start']);
+            if(is_null($this_house['nextOpenHouse'])){
+              $this_house['nextOpenHouse'] = $timestamp;
+            } else {
+              $this_house['nextOpenHouse'] = min($timestamp, $this_house['nextOpenHouse']);
+            }
+            $upcomingOpenHouses[] = $timestamp;
+          }
+        }
+        $this_house['upcomingOpenHouses'] = implode(',', $upcomingOpenHouses);
+        $this_house['size_total'] = $this_house['livingArea'] + $this_house['supplementalArea'];
+        $this_house['created'] = time();
+
+        // Assoc ID with key as house ID in case a house comes up more than once in different searches, prevents duplication
+        $houses[$this_house['id']] = $this_house;
+
+      }
+
+      // Push up the offset for the next loop
+      $api_offset += $api_limit;
     }
   }
 
@@ -109,19 +186,17 @@ function fetch_hemnet_houses($search_id = false){
 
   // Save each house to the db
   foreach($houses as $id => $house){
+    // Remove null values
+    $house = array_filter($house, function($value) { return !is_null($value); });
+
+    // Prep SQL statement
     $sql = '
       INSERT INTO `houses` (`'.implode('`, `', array_keys($house)).'`)
       VALUES ("'.implode('", "', $house).'")';
-    if(!$mysqli->query($sql)) return array("status"=>"error", "msg" => "Could not insert house $id into DB:<br>$sql<br>".$mysqli->error);
+    if(!$mysqli->query($sql)) return array("status"=>"error", "msg" => "Could not insert house $id into DB!", "sql" => $sql, "sql_error" => $mysqli->error, "house" => $house);
   }
 
   return array("status"=>"success", "msg" => "Found ".count($houses)." houses");
-}
-
-function _clean_num($str){
-  $cleaned = str_replace(',', '.', preg_replace('/[^0-9,]/', '', $str));
-  if($cleaned == '') return 0;
-  return $cleaned;
 }
 
 
@@ -139,4 +214,172 @@ if ( basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"]) ) {
 
   echo json_encode(fetch_hemnet_houses($search_id), JSON_PRETTY_PRINT);
 
+}
+
+
+
+function graphql_iOSSearchQuery(){
+    // This query returns as few fields as possible - only the stuff that we may want to filter on in the DB
+    // Everything else can be fetched when a single house is clicked on in the map.
+    return '
+      query iOSSearchQuery($searchInput: ListingSearchInput!, $limit: Int!, $offset: Int, $sort: ListingSearchSorting) {
+        searchListings(search: $searchInput, limit: $limit, offset: $offset, sort: $sort) {
+          total
+          offset
+          limit
+          listings {
+            ...PartialPropertyListingFragment
+          }
+        }
+      }
+
+      fragment MoneyFragment on Money {
+        amount
+      }
+
+      fragment PartialPropertyListingFragment on PropertyListing {
+        id
+        type: primaryTypeGroup
+        isUpcoming
+
+        coordinates {
+          lat
+          long
+        }
+
+        ... on ProjectUnit {
+          askingPrice {
+            ...MoneyFragment
+          }
+          runningCosts {
+            ...MoneyFragment
+          }
+
+          livingArea
+          landArea
+          supplementalArea
+          daysOnHemnet
+          isBiddingOngoing
+          numberOfRooms
+
+          upcomingOpenHouses {
+            start
+          }
+        }
+
+        ... on ActivePropertyListing {
+          askingPrice {
+            ...MoneyFragment
+          }
+          runningCosts {
+            ...MoneyFragment
+          }
+
+          livingArea
+          landArea
+          supplementalArea
+          daysOnHemnet
+          isNewConstruction
+          isForeclosure
+          isBiddingOngoing
+          numberOfRooms
+
+          upcomingOpenHouses {
+            start
+          }
+        }
+      }
+    ';
+
+    // The older query with more fields. We may want to look at this again for reference later? Can delete soon though.
+    $old_query = '
+    query iOSSearchQuery($searchInput: ListingSearchInput!, $limit: Int!, $offset: Int, $sort: ListingSearchSorting) {
+      searchListings(search: $searchInput, limit: $limit, offset: $offset, sort: $sort) {
+        total
+        offset
+        limit
+
+        listings {
+          ...PartialPropertyListingFragment
+          labels {
+            text
+            category
+            identifier
+          }
+        }
+      }
+    }
+
+    fragment MoneyFragment on Money {
+      amount
+    }
+
+    fragment PartialPropertyListingFragment on PropertyListing {
+      id
+      type: primaryTypeGroup
+      area
+      legacyPrimaryLocation
+      isUpcoming
+      streetAddress
+
+      coordinates {
+        lat
+        long
+      }
+
+      ... on Project {
+        publishedAt
+      }
+
+      ... on ProjectUnit {
+        askingPrice {
+          ...MoneyFragment
+        }
+        runningCosts {
+          ...MoneyFragment
+        }
+
+        livingArea
+        landArea
+        supplementalArea
+        daysOnHemnet
+        publishedAt
+        numberOfRooms
+
+        upcomingOpenHouses {
+          start
+        }
+
+        isBiddingOngoing
+      }
+
+      ... on ActivePropertyListing {
+        askingPrice {
+          ...MoneyFragment
+        }
+        runningCosts {
+          ...MoneyFragment
+        }
+
+        livingArea
+        landArea
+        supplementalArea
+        daysOnHemnet
+        publishedAt
+        legacyConstructionYear
+        tenure {
+          name
+        }
+        description
+        isNewConstruction
+        isForeclosure
+        isBiddingOngoing
+        listingBrokerUrl
+        numberOfRooms
+
+        upcomingOpenHouses {
+          start
+        }
+      }
+    }';
 }
